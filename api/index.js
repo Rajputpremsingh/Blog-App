5d90cdbeadc03bf7388f466ee97d742577c3c3f0
Kajalmondal1 <kajalmondal148@gmail.com>
2024-06-02 11:40:22 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js";   // We can import by different name as well like here we have imported "router" as "userRoutes"
 dotenv.config()
 
 const app = express()
+app.use(userRoutes)
 
 mongoose.connect(process.env.MONGO_URL).then(
     ()=>{
